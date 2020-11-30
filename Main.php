42ea271cfceded6e7764b8f25b45f7219a0855a6
unknown <s192235>
2020-11-30 15:27:09 +0900

    SDK 追加

--- Main.php
+++ Main.php
@@ -1,12 +1,11 @@
 //例としてPexels(https://www.pexels.com/ja-jp/)の犬の画像のURLを使用
 
 <?php
+
+//https://www.instagram.com/explore/tags/[ハッシュタグ名]/
+
 $url = 'https://images.pexels.com/photos/1287830/pexels-photo-1287830.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260';
 
 
-$data = file_get_contents($url);
-
-// 以下は同様にブラウザに出力
-header('Content-type: image/jpg');
-echo $data;
-?>
+<a href=$url><img src="images/Dog.jpg" width=100 height=100></a>
+		

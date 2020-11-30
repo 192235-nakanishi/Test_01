40b45b402bcb5c81e763e5b85dc59608c63a6594
unknown <s192235>
2020-11-30 16:39:26 +0900

    php書き換え

--- Main.php
+++ Main.php
@@ -3,9 +3,5 @@
 <?php
 
 //https://www.instagram.com/explore/tags/[ハッシュタグ名]/
-
-$url = 'https://images.pexels.com/photos/1287830/pexels-photo-1287830.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260';
-
-
-<a href=$url><img src="images/Dog.jpg" width=100 height=100></a>
-		
+//InstagramAPIを使わない場合のURL
+	?>	
